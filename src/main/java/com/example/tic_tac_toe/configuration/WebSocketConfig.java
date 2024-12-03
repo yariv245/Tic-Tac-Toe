d3e1a89bc7204c49dfac44a4f45dd773beee4ec2
Yariv Menachem <yariv220895@gmail.com>
2024-12-03 16:26:30 +0200

    added timeout of 1 minute

--- src/main/java/com/example/tic_tac_toe/configuration/WebSocketConfig.java
+++ src/main/java/com/example/tic_tac_toe/configuration/WebSocketConfig.java
@@ -3,10 +3,14 @@ package com.example.tic_tac_toe.configuration;
 import com.example.tic_tac_toe.handler.CustomErrorHandler;
 import com.example.tic_tac_toe.handler.TicTacToeWebSocketHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
+
+import java.time.Duration;
 
 @Configuration
 @EnableWebSocket
@@ -17,6 +21,15 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new CustomErrorHandler(webSocketHandler), "/tictactoe").setAllowedOrigins("*");
+        registry.addHandler(new CustomErrorHandler(webSocketHandler), "/tictactoe")
+                .setAllowedOrigins("*");
+    }
+
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxSessionIdleTimeout(Duration.ofMinutes(1).toMillis());
+
+        return container;
     }
 }
